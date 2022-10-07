3215f2f4b30f36b1be8f5e8d799408d75bcec2ea
jenbuzz <daniel_lynge@hotmail.com>
2022-10-07 14:54:40 +0200

    updated react render snippet for react 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-function renderApp() {
-    const App = require('./App').default;
-    render(<App />, document.getElementById('app'));
-}
-
-renderApp();
-
-if (module.hot) {
-    module.hot.accept(renderApp);
-}
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
